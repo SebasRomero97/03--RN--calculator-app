f3d4a91d89fda73f0ea66e85613169e0f5a75113
SebasRomero <sebas_3233@hotmail.com>
2025-05-06 19:56:47 -0300

    Se borro lo que no servia

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import Pepe from './pepe'
+
+const CalculatorApp = () => {
+  return (
+    <View>
+      <Text>CalculatorApp</Text>
+      <Pepe />
+      <Text>Footer index</Text>
+    </View>
+  )
+}
+
+export default CalculatorApp
---
+++ app/pepe.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Pepe = () => {
+  return (
+    <View>
+      <Text>Pepe</Text>
+    </View>
+  )
+}
+
+export default Pepe
